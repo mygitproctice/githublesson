884f594b10d4cd2dc740d45b933f3ad67608a23a
BLR\narasimhacp12302 <alurunarasimha@gmail.com>
2017-12-10 00:11:24 +0530

    Intial Commit

---
+++ com.test/src/main/java/dotest/com/test/Test1.java
@@ -0,0 +1,11 @@
+package dotest.com.test;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+  @Test
+  public void f() {
+	  
+	  System.out.println("Test");
+  }
+}
---
+++ com.test/src/main/java/dotest/com/test/Test2.java
@@ -0,0 +1,11 @@
+package dotest.com.test;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+  @Test
+  public void f() {
+	  
+	  System.out.println("test2");
+  }
+}
